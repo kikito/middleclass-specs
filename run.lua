f7fcf2c6172c359658a4903972aec19abeea8049
Enrique García <kikito@gmail.com>
2010-11-29 00:28:33 +0100

    massive renaming

--- run.lua
+++ run.lua
@@ -1,4 +1,4 @@
 #!/usr/bin/env lua
 
 local package_path = "package.path = '?.lua;?/init.lua;'"
-os.execute( string.format("tsc -f --before=%q *_spec.lua", package_path ) )
+os.execute( string.format("tsc -f --before=%q core_*.lua extras_*.lua", package_path ) )
