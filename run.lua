516cc1fb48ddf6364f7f457b02430023cbd78d0a
Enrique García <kikito@gmail.com>
2010-11-30 00:22:49 +0100

    moved submodules inside a lib/ folder

--- run.lua
+++ run.lua
@@ -1,4 +1,4 @@
 #!/usr/bin/env lua
 
-local package_path = "package.path = '?.lua;?/init.lua;'"
-os.execute( string.format("tsc -f --before=%q core_*.lua extras_*.lua", package_path ) )
+local package_path_command = "package.path = 'lib/?.lua;lib/?/init.lua;' .. package.path"
+os.execute( ("tsc -f --before=%q core_*.lua extras_*.lua"):format( package_path_command ) )
