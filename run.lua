795b7abd4c5e09ab01c6f9f7f29f01519200fe5d
Enrique García <kikito@gmail.com>
2010-11-30 01:26:09 +0100

    updated submodules and run.lua

--- run.lua
+++ run.lua
@@ -1,4 +1,4 @@
 #!/usr/bin/env lua
 
-local package_path_command = "package.path = 'lib/?.lua;lib/?/init.lua;' .. package.path"
+local package_path_command = "package.path = '?.lua;lib/?.lua;lib/?/init.lua;' .. package.path"
 os.execute( ("tsc -f --before=%q core_*.lua extras_*.lua"):format( package_path_command ) )
